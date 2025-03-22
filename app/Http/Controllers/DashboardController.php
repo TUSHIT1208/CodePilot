61216e8c6c3b1e7f7f679ed668e54b2238640690
unknown <tusheetm@gmail.com>
2025-03-22 14:04:17 +0530

    tushit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -73,43 +73,6 @@ class DashboardController extends Controller
         return view('admin.report.total_course.list');
     }
 
-
-    // public function learner(Request $request)
-    // {
-    //     if ($request->ajax()) {
-    //         $learners = User::select([
-    //             'id',
-    //             'profile_picture_url',
-    //             'first_name',
-    //             'middle_name',
-    //             'last_name',
-    //             'email',
-    //             'phone_number',
-    //             'is_active'
-    //         ])->where('role_id', 3);
-
-    //         return DataTables::of($learners)
-    //             ->addColumn('profile', function ($learner) {
-    //                 return !empty($learner->profile_picture_url)
-    //                     ? '<img id="profile_picture" src="' . asset($learner->profile_picture_url) . '" width="40">'
-    //                     : '<h1 id="default_avtar">' . strtoupper(substr($learner->first_name, 0, 1)) . '</h1>';
-    //             })
-    //             ->addColumn('full_name', function ($row) {
-    //                 return $row->first_name . ' ' . ($row->middle_name ? $row->middle_name . ' ' : '') . $row->last_name;
-    //             })
-    //             ->addColumn('status', function ($row) {
-    //                 return $row->is_active ? '<span class="badge badge-success active-learner">Active</span>' : '<span class="badge badge-danger inctive-learner">Inactive</span>';
-    //             })
-    //             ->rawColumns(['profile', 'status'])
-    //             ->make(true);
-    //     }
-
-    //     $learners = User::where('role_id', 3)->get();
-    //     $categories = category::all();
-    //     $userCourses = user_course::get();
-    //     return view('admin.report.total_learner.list', compact('learners','categories'));
-    // }
-
     public function learner(Request $request)
     {
         if ($request->ajax()) {
